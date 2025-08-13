65985be54754a605faf3981dd4101e2090eaf0ce
RaZooM <160235728+RazoomGD@users.noreply.github.com>
2025-08-14 02:31:06 +0300

    fix grid snap bug

--- src/EditorUI.cpp
+++ src/EditorUI.cpp
@@ -7,4 +7,25 @@ class $modify(MyEditorUI, EditorUI) {
 		EditorUI::updateZoom(p0);
 		DrawGridAPI::get().markDirty();
 	}
+
+	// fix bug when grid snap breaks the duration lines
+	void ccTouchEnded(CCTouch* p0, CCEvent* p1) {
+		bool shouldUpdateEndPos = (
+			p0->m_nId != m_rotationTouchID && 
+			p0->m_nId != m_scaleTouchID && 
+			p0->m_nId != m_transformTouchID && 
+			p0->m_nId == m_touchID && 
+			m_snapObjectExists && 
+			m_continuousSnap && 
+			m_snapObject && 
+			GameManager::get()->getGameVariable("0008") && 
+			(int)m_snapObject->getRotation() % 90 == 0
+		);
+	
+		EditorUI::ccTouchEnded(p0, p1);
+	
+		if (shouldUpdateEndPos) {
+			m_editorLayer->m_drawGridLayer->m_updateTimeMarkers = true;
+		}
+	}
 };
