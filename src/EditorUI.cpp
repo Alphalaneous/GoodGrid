d3f477e3ab7ce4129e9b1cfa5e32e989fe919eca
RaZooM <160235728+RazoomGD@users.noreply.github.com>
2025-08-14 02:35:11 +0300

    fix fix grid snap bug

--- src/EditorUI.cpp
+++ src/EditorUI.cpp
@@ -2,6 +2,8 @@
 #include <Geode/modify/EditorUI.hpp>
 #include "../include/DrawGridAPI.hpp"
 
+using namespace geode::prelude;
+
 class $modify(MyEditorUI, EditorUI) {
 	void updateZoom(float p0) {
 		EditorUI::updateZoom(p0);
